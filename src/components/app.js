6e420ab3fef638ee2781083869ab10658475297f
DictumMortuum <theio.vrefos@gmail.com>
2017-03-08 16:58:55 +0200

    readme

--- src/components/app.js
+++ src/components/app.js
@@ -19,25 +19,6 @@ const muiTheme = getMuiTheme({
   }
 });
 
-/*
-lightBaseTheme:
-primary1Color: cyan500,
-primary2Color: cyan700,
-primary3Color: grey400,
-accent1Color: pinkA200,
-accent2Color: grey100,
-accent3Color: grey500,
-textColor: darkBlack,
-secondaryTextColor: fade(darkBlack, 0.54),
-alternateTextColor: white,
-canvasColor: white,
-borderColor: grey300,
-disabledColor: fade(darkBlack, 0.3),
-pickerHeaderColor: cyan500,
-clockCircleColor: fade(darkBlack, 0.07),
-shadowColor: fullBlack,
-*/
-
 window.onscroll = function () {
   if (document.body.scrollHeight - document.body.scrollTop === document.body.clientHeight) {
     store.dispatch(scrollDocs());
