71e26a81a89cb10670ce1f897c5b44de2db0998a
DictumMortuum <theio.vrefos@gmail.com>
2017-03-09 14:06:55 +0200

    fixing production config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,18 +1,18 @@
 'use strict';
 
-var path = require('path');
-var node_modules_dir = path.resolve(__dirname, 'node_modules');
+let path = require('path');
+let node = path.resolve(__dirname, 'node_modules');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: __dirname,
     filename: 'bundle.js'
   },
   module: {
     loaders: [{
       test: path.join(__dirname, 'src'),
-      exclude: [node_modules_dir],
+      exclude: [node],
       loader: 'babel-loader'
     }]
   }
