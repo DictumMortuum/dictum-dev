23b9f7cd4d25a665d343f317fc0657fc1265bc01
DictumMortuum <theio.vrefos@gmail.com>
2017-03-10 15:58:51 +0200

    edge case error prevention when fetch returns no docs

--- src/redux/reducers/date.js
+++ src/redux/reducers/date.js
@@ -10,10 +10,14 @@ const defaultState = {
 export default (state=defaultState, action) => {
   switch (action.type) {
   case 'DOCS_FETCH':
-    return {
-      to: action.docs[0]._id,
-      from: action.docs[action.docs.length - 1]._id
-    };
+    if (action.docs.length > 0) {
+      return {
+        to: action.docs[0]._id,
+        from: action.docs[action.docs.length - 1]._id
+      };
+    } else {
+      return state;
+    }
   // TODO maybe add DOC_INSERT and DOC_DELETE cases.
   default:
     return state;
