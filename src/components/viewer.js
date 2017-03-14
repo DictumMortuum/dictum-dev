46e9cc92897fd052efdec643cccae96ccf249d4a
DictumMortuum <theio.vrefos@gmail.com>
2017-03-14 15:50:24 +0200

    using reselect to remove logic from the viewer component

--- src/components/viewer.js
+++ src/components/viewer.js
@@ -4,20 +4,20 @@ import React from 'react';
 import Doc from './doc';
 import Paper from 'material-ui/Paper';
 import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
 
 let Viewer = React.createClass({
   propTypes: {
     docs: React.PropTypes.array,
-    config: React.PropTypes.object,
-    length: React.PropTypes.number
+    config: React.PropTypes.object
   },
 
   render() {
-    let { docs, config, length } = this.props;
+    let { docs, config } = this.props;
 
     return (
       <Paper zDepth={0} style={{padding: 10, marginTop: 64}} >
-        {docs.slice(0, length).map((d, index) => {
+        {docs.map((d, index) => {
           return (<Doc key={d._id} doc={d} config={config} index={index} />);
         })}
       </Paper>
@@ -25,8 +25,20 @@ let Viewer = React.createClass({
   }
 });
 
-export default connect(state => ({
+const mapStateToProps = state => ({
   docs: state.docs,
   config: state.config,
   length: state.length
-}))(Viewer);
+});
+
+const mergeProps = createSelector(
+  state => state.docs,
+  state => state.config,
+  state => state.length,
+  (docs, config, length) => ({
+    config,
+    docs: docs.slice(0, length)
+  })
+);
+
+export default connect(mapStateToProps, {}, mergeProps)(Viewer);
