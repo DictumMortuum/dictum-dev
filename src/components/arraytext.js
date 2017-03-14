50f17322abff8dd36a0245cc86521d16ed313edc
DictumMortuum <theio.vrefos@gmail.com>
2017-03-14 18:00:26 +0200

    investigating a way to merge ArrayText and Text using reselect

--- src/components/arraytext.js
+++ src/components/arraytext.js
@@ -2,33 +2,34 @@
 
 import React from 'react';
 import TextField from 'material-ui/TextField';
-import store from '../redux/store';
+import { connect } from 'react-redux';
 import { Editor } from '../redux/actions';
 import { textStyle } from '../styles';
+import { createSelector } from 'reselect';
 
-export default React.createClass({
-  propTypes: {
-    id: React.PropTypes.string,
-    hint: React.PropTypes.string,
-    value: React.PropTypes.array
-  },
-
-  handleChange(event, value) {
-    store.dispatch(Editor.change(this.props.id, value.split(',')));
-  },
-
+let ArrayText = React.createClass({
   render() {
-    let { value } = this.props;
-
-    return (
-      <TextField
-        id={this.props.id}
-        key={this.props.id}
-        hintText={this.props.hint}
-        value={value.toString()}
-        onChange={this.handleChange}
-        style={textStyle}
-      />
-    );
+    console.log(this.props);
+    return (<TextField {...this.props} style={textStyle} />);
   }
 });
+
+const mapStateToProps = (state, props) => props;
+
+const mapDispatchToProps = {
+  change: Editor.change
+};
+
+const mergeProps = createSelector(
+  props => props,
+  (state, actions) => actions.change,
+  (props, change) => ({
+    id: props.id,
+    key: props.id,
+    hintText: props.hint,
+    value: props.value.toString(),
+    onChange: (event, value) => change(props.id, value.split(','))
+  })
+);
+
+export default connect(mapStateToProps, mapDispatchToProps, mergeProps)(ArrayText);
