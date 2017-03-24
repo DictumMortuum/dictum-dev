178e9aa24dfaa8a2540597ce5263f9cc2cde31a1
DictumMortuum <theio.vrefos@gmail.com>
2017-03-24 15:20:20 +0200

    simplifying doc

--- src/components/doc.js
+++ src/components/doc.js
@@ -10,6 +10,66 @@ import { chipStyle, font } from '../styles';
 import Jira from './jira';
 import { CompanyChip, DateChip, ProductChip, TypeChip } from './chip';
 
+let DocLang = React.createClass({
+  propTypes: {
+    lang: React.PropTypes.string
+  },
+
+  render() {
+    let { lang } = this.props;
+
+    return (
+      <RaisedButton
+        label={lang}
+        style={font}
+        onTouchTap={() => store.dispatch(Filter.toggle(lang))}
+      />
+    );
+  }
+});
+
+let DocActions = React.createClass({
+  propTypes: {
+    ticket: React.PropTypes.string,
+    lang: React.PropTypes.array
+  },
+
+  render() {
+    let { ticket, lang } = this.props;
+
+    return (
+      <CardActions>
+        {ticket && <Jira ticket={ticket} />}
+        {lang && lang.map(l => (<DocLang key={l} lang={l} />))}
+      </CardActions>
+    );
+  }
+});
+
+let DocHeader = React.createClass({
+  propTypes: {
+    doc: React.PropTypes.object,
+    config: React.PropTypes.object
+  },
+
+  render() {
+    let { doc, config } = this.props;
+
+    return (
+      <div style={chipStyle}
+        onTouchTap={() => store.dispatch(Doc.edit(doc))}>
+        {DateChip(new Date(doc.updated || doc._id).toLocaleDateString(config.locale, {
+          year: 'numeric', month: 'numeric', day: 'numeric',
+          hour: '2-digit', minute: '2-digit', second: '2-digit'
+        }))}
+        {TypeChip(doc.type)}
+        {CompanyChip(doc.company)}
+        {ProductChip(doc.product)}
+      </div>
+    );
+  }
+});
+
 export default React.createClass({
   propTypes: {
     doc: React.PropTypes.object,
@@ -17,32 +77,15 @@ export default React.createClass({
   },
 
   render() {
-    let { config, doc } = this.props;
+    let { doc } = this.props;
 
     return (
       <Card style={{marginBottom: 20}}>
-        <div style={chipStyle}
-          onTouchTap={() => store.dispatch(Doc.edit(doc))}>
-          {DateChip(new Date(doc.updated || doc._id).toLocaleDateString(config.locale, {
-            year: 'numeric', month: 'numeric', day: 'numeric',
-            hour: '2-digit', minute: '2-digit', second: '2-digit'
-          }))}
-          {TypeChip(doc.type)}
-          {CompanyChip(doc.company)}
-          {ProductChip(doc.product)}
-        </div>
+        <DocHeader {...this.props} />
         <CardText>
           <ReactMarkdown source={doc.desc || ''} />
         </CardText>
-        <CardActions>
-          {doc.ticket && <Jira ticket={doc.ticket} />}
-          {doc.lang && doc.lang.map(l => (<RaisedButton
-            key={l}
-            label={l}
-            style={font}
-            onTouchTap={() => store.dispatch(Filter.toggle(l))}
-          />))}
-        </CardActions>
+        <DocActions ticket={doc.ticket} lang={doc.lang} />
       </Card>
     );
   }
