52f391aa94c69bce3c0bb1f6cf693f2bdda18783
DictumMortuum <theio.vrefos@gmail.com>
2017-03-24 10:46:11 +0200

    removing moment.js

--- src/redux/reducers/date.js
+++ src/redux/reducers/date.js
@@ -1,14 +1,22 @@
 'use strict';
 
-import moment from 'moment';
 import { sort, cast } from '../db';
 
-const defaultState = {
-  from: moment(new Date()).startOf('day').subtract(30, 'days').toISOString(),
-  to: moment(new Date()).startOf('day').toISOString()
+const defaultState = () => {
+  let to = new Date();
+  let from = new Date(to);
+  // start of day
+  from.setHours(0, 0, 0, 0);
+  // 30 days ago
+  from.setDate(to.getDate() - 30);
+
+  return {
+    to: to.toISOString(),
+    from: from.toISOString()
+  };
 };
 
-export default (state=defaultState, action) => {
+export default (state=defaultState(), action) => {
   switch (action.type) {
   case 'DOCS_FETCH':
     if (action.docs.length > 0) {
