b62c87228e445c240d4819c9e9f8de727fb48e7b
DictumMortuum <theio.vrefos@gmail.com>
2017-03-25 20:03:24 +0200

    doc header

--- src/components/doc.js
+++ src/components/doc.js
@@ -2,57 +2,14 @@
 
 import React from 'react';
 import { Card, CardText, CardActions } from 'material-ui/Card';
-import store from '../redux/store';
-import { Doc } from '../redux/actions';
 import ReactMarkdown from 'react-markdown';
-import { chipStyle } from '../styles';
 import Jira from './doc/jira';
 import Filter from './doc/filter';
-import { CompanyChip, DateChip, ProductChip, TypeChip } from './doc/chip';
+import Header from './doc/header';
+import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
 
-let DocActions = React.createClass({
-  propTypes: {
-    ticket: React.PropTypes.string,
-    lang: React.PropTypes.array
-  },
-
-  render() {
-    let { ticket, lang } = this.props;
-
-    return (
-      <CardActions>
-        {ticket && <Jira ticket={ticket} />}
-        {lang && lang.map(l => (<Filter key={l} lang={l} />))}
-      </CardActions>
-    );
-  }
-});
-
-let DocHeader = React.createClass({
-  propTypes: {
-    doc: React.PropTypes.object,
-    config: React.PropTypes.object
-  },
-
-  render() {
-    let { doc, config } = this.props;
-
-    return (
-      <div style={chipStyle}
-        onTouchTap={() => store.dispatch(Doc.edit(doc))}>
-        {DateChip(new Date(doc.updated || doc._id).toLocaleDateString(config.locale, {
-          year: 'numeric', month: 'numeric', day: 'numeric',
-          hour: '2-digit', minute: '2-digit', second: '2-digit'
-        }))}
-        {TypeChip(doc.type)}
-        {CompanyChip(doc.company)}
-        {ProductChip(doc.product)}
-      </div>
-    );
-  }
-});
-
-export default React.createClass({
+const template = React.createClass({
   propTypes: {
     doc: React.PropTypes.object
   },
@@ -62,12 +19,30 @@ export default React.createClass({
 
     return (
       <Card style={{marginBottom: 20}}>
-        <DocHeader {...this.props} />
+        <Header doc={doc} />
         <CardText>
-          <ReactMarkdown source={doc.desc || ''} />
+          <ReactMarkdown source={doc.desc} />
         </CardText>
-        <DocActions ticket={doc.ticket} lang={doc.lang} />
+        <CardActions>
+          {doc.ticket && <Jira ticket={doc.ticket} />}
+          {doc.lang.map(l => (<Filter key={l} lang={l} />))}
+        </CardActions>
       </Card>
     );
   }
 });
+
+export default connect(
+  (state, props) => ({ props }),
+  {},
+  createSelector(
+    state => state.props,
+    props => ({
+      doc: {
+        ...props.doc,
+        desc: props.doc.desc || '',
+        lang: props.doc.lang || []
+      }
+    })
+  )
+)(template);
