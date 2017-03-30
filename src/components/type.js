81bbd70130f801f06423a293259654b3772e197f
DictumMortuum <theio.vrefos@gmail.com>
2017-03-30 16:23:28 +0300

    smaller type input in bar

--- src/components/type.js
+++ src/components/type.js
@@ -43,7 +43,8 @@ export default connect(
       types: type.types,
       select: {
         style: {
-          marginRight: 10
+          marginRight: 10,
+          width: 200
         },
         labelStyle: {
           color: 'white'
